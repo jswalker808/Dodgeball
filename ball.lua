ef341401ea32f369011061ac9b14b525f5dab2ce
James Walker <AAWH8ZZ@mmm.com>
2020-10-22 23:27:21 -0600

    Adds ball class, adds smile to player

---
+++ ball.lua
@@ -0,0 +1,20 @@
+Ball = Class{}
+
+function Ball:init (x, y, height, width)
+    self.x = x
+    self.y = y
+
+    self.height = height
+    self.width = width
+
+    self.dx = 0
+    self.dy = 0
+end
+
+function Ball:update(dt)
+    if self.dx < 0 then
+        self.x = math.max(0, self.x + self.dx * dt)
+    else
+        self.x = math.min(VIRTUAL_WIDTH - self.width, self.x + self.dx * dt)
+    end
+end
--- main.lua
+++ main.lua
@@ -60,12 +60,18 @@ function love.draw()
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
-    -- Render the player
-
+    -- Render the body
     love.graphics.rectangle('fill', player.x, player.y, player.width, player.height)
     love.graphics.setColor(40/255, 45/255, 52/255, 255/255)
+
+    -- Render the eyes
     love.graphics.rectangle('fill', player.x + 3, player.y + 5, 2, 2)
     love.graphics.rectangle('fill', player.x + player.width - 5, player.y + 5, 2, 2)
+
+    -- Render the mouth
+    love.graphics.rectangle('fill', player.x + 3, player.y + 10, 1, 2)
+    love.graphics.rectangle('fill', player.x + 3, player.y + 12, player.width - 6, 1)
+    love.graphics.rectangle('fill', player.x + player.width - 4, player.y + 10, 1, 2)
 
     push:apply('end')
 end
